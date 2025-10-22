76c235515d83888245ef3d219425e5618e89000b
Rafiul Islam <cmmnacrafiulislam0170@gmail.com>
2025-10-23 05:49:40 +0600

    init(apps/chat): initialize chat app

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party packages
+    'rest_framework',
+
+    # Custom apps
+    'apps.chat',
 ]
 
 MIDDLEWARE = [
