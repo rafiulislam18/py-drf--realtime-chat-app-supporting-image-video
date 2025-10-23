bd3a036e49c2a5e1c72b728b6e0c98481b651146
Rafiul Islam <cmmnacrafiulislam0170@gmail.com>
2025-10-23 06:06:58 +0600

    feat(utils/exceptions): implement custom exception handler for better default error response format

--- config/settings.py
+++ config/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    "EXCEPTION_HANDLER": "utils.exceptions.custom_exception_handler",
+}
